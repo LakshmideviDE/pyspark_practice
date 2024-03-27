d5568c018e1db6f528c7a8145857a9fa768b0215
lakshmidevig <lakshmidevig870@gmail.com>
2024-03-27 12:53:59 +0530

    creating dataframe

---
+++ dataframe/main.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+# Create a SparkSession
+spark = (SparkSession.builder\
+    .appName("Simple Example of pyspark")\
+         .getOrCreate())
+data = [(1, "lakshmi"), (2, "chandu")]
+
+# schema = ["id", "name"]
+schema = StructType([StructField(name='id', dataType=IntegerType()),
+                     StructField(name='name', dataType=StringType())])
+
+
+# Create DataFrame
+
+df = spark.createDataFrame(data=data,schema=schema)
+df.show()
+df1 = df.count()
+
+
+
+
+
+
+
+
