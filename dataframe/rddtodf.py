9b8d642cb14e2317f17e9fdae359eb0334a2bc38
lakshmidevig <lakshmidevig870@gmail.com>
2024-03-27 17:57:04 +0530

    converting rdd to dataframe

--- dataframe/rddtodf.py
+++ dataframe/rddtodf.py
@@ -5,3 +5,4 @@ spark = SparkSession.builder.appName("creating of data").getOrCreate()
 df = spark.read.csv(r"C:\Users\Lakshmidevi\PycharmProjects\psyspark\pyspark_practice\rdd\first.csv", header=True, inferSchema=True)
 #df.select("name").show()
 df.printSchema()
+#
