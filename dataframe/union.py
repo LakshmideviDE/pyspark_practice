ed9c2aeb287e2c17b0be798c6c4742be4529d16a
lakshmidevig <lakshmidevig870@gmail.com>
2024-04-03 16:39:05 +0530

    to perform union and unionall

---
+++ dataframe/union.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+spark = SparkSession.builder.appName("creating of data").getOrCreate()
+
+df1 = spark.read.csv(r"C:\Users\Lakshmidevi\PycharmProjects\psyspark\pyspark_practice\dataframe\u1.csv",header=True,inferSchema=True)
+df2 = spark.read.csv(r"C:\Users\Lakshmidevi\PycharmProjects\psyspark\pyspark_practice\dataframe\u2.csv",header=True,inferSchema=True)
+# df=df1.printSchema()
+# df=df2.printSchema()
+df=df1.union(df2)
+df.show()
+df=df1.union(df2).distinct()
+df.show()
+df=df1.unionAll(df2)
+df.show()
